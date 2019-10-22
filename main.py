fc76ee635d9b0c66574e07dc7600c5ba9efe17f0
Olmo Cerri <olmo.cerri@gmail.com>
2019-10-21 18:28:36 -0700

    debug lines removed

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
 from kivy.utils import platform
 
+sys.path.append(".")
 if platform == 'ios':
 	sys.path.append("_applibs")
-sys.path.append(".")
-# sys.path.append("iOS_libraries")
+	sys.path.append("iOS_libraries")
 
 from JumpyKitten import JumpyKittenPage
 from mainPage import mainPage
