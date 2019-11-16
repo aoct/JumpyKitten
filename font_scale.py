ea4899dddbd9bb53de85768723be2c92afcf32fc
aletambo <alessiotamborini@gmail.com>
2019-11-16 09:38:13 -0800

    text sizing

--- font_scale.py
+++ font_scale.py
@@ -7,13 +7,14 @@ from os.path import join
 import os, pickle, time
 
 def font_scaling(num, scale=1):
-	return sp(num*scale)
+	return  num*scale
 
-def scaling(text_size = 84.0):
-	computer_size = 84.0 #label.text_size[0] on my macbook pro
-	phone_size = text_size #label.text_size[0] on my handheld device
+def scaling(pixel_screen_width = 84.0):
+	computer_size = 1170. #label.text_size[0] on my macbook pro
+	phone_size = pixel_screen_width #label.text_size[0] on my handheld device
 	calculated_scale = phone_size/computer_size
-	print('[DEBUG]: Font scaling: {:.1f}/{:.1f} = {:.1f}'.format(text_size, computer_size, calculated_scale))
+	print(computer_size, phone_size, calculated_scale)
+	print('[DEBUG]: Font scaling: {:.1f}/{:.1f} = {:.1f}'.format(phone_size, computer_size, calculated_scale))
 
 	if platform == 'ios': user_data_dir = App.get_running_app().user_data_dir
 	else: user_data_dir = 'data'
