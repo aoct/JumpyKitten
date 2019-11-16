da6a23b927e20b462e357b05b88500ccbd72a27b
Olmo Cerri <olmo.cerri@gmail.com>
2019-11-16 10:22:03 -0800

    New version

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os, sys
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 import kivy
-kivy.require("1.10.1")
+kivy.require("1.10.0")
 from kivy.utils import platform
 
 if platform != 'ios' and platform != 'android':
